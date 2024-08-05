92f823d15eee1cca593be63d96c4fa0c75f9c1d1
Oriana Mendez <egleemendez@gmail.com>
2024-08-05 12:07:52 +0200

    add button and book card components

--- src/app/design-system/page.tsx
+++ src/app/design-system/page.tsx
@@ -1,3 +1,14 @@
-function DesignSystemPage() {
-  return <h1>Here are the components</h1>;
+import { CTAButton, DeleteButton } from "@/components/buttons";
+import { BookCards } from "@/components/cards";
+import { TopNavPage } from "@/components/top-nav";
+
+export default function DesignSystemPage() {
+  return (
+    <>
+      <TopNavPage />
+      <CTAButton />
+      <DeleteButton />
+      <BookCards />
+    </>
+  );
 }

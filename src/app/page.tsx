f61805e4096dfda6df36c8f8b3a7616888038689
Oriana Mendez <egleemendez@gmail.com>
2024-08-05 15:13:14 +0200

    gh-21 add bookcard component in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
+import {
+  charlotteWebBook,
+  theBfgBook,
+  theMagicTreeHouseBook,
+} from "@/components/books";
+import { BookCards } from "@/components/cards";
 import { Description } from "@/components/description";
 import { TopNavPage } from "@/components/top-nav";
-import Image from "next/image";
 
 export default function Home() {
   return (
@@ -8,24 +13,11 @@ export default function Home() {
       <TopNavPage />
       <main className="">
         <Description />
-        <article>
-          <section>
-            <h2>Book name</h2>
-            <p>Description of the book</p>
-            <p>Points</p>
-          </section>
-          <section>
-            <h2>Book name</h2>
-            <p>Description of the book</p>
-            <p>Points</p>
-          </section>
-          <section>
-            <h2>Book name</h2>
-            <p>Description of the book</p>
-            <p>Points</p>
-          </section>
-        </article>
-
+        <div className="flex gap-4 p-4 pt-8 justify-center">
+          <BookCards {...charlotteWebBook} />
+          <BookCards {...theMagicTreeHouseBook} />
+          <BookCards {...theBfgBook} />
+        </div>
         <form action="">
           <label>Book</label>
           <input type="text" />

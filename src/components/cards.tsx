b1b77e50ceed9a6230b99b285272b69ea48e8938
Oriana Mendez <egleemendez@gmail.com>
2024-08-05 15:08:36 +0200

    gh-22 add params

--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -1,18 +1,41 @@
 import Image from "next/image";
 
-export function BookCards() {
+export function BookCards({
+  name,
+  author,
+  description,
+  pages,
+  image,
+  points,
+}: {
+  name: string;
+  author: string;
+  description: string;
+  pages: number;
+  image: string;
+  points: number;
+}) {
   return (
     <div className="card bg-base-100 w-96 shadow-xl">
       <figure>
-        <Image width={80} height={80} src="" alt="Shoes" />
+        <Image width={150} height={150} src={image} alt="Shoes" />
       </figure>
       <div className="card-body">
-        <h2 className="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <h2 className="card-title">{name}</h2>
+        <p>
+          <strong>Author: </strong>
+          {author}
+        </p>
+        <p className="pt-2 pb-2">{description}</p>
         <div className="card-actions justify-end">
-          <button className="btn bg-yellow-300 hover:bg-yellow-200">
-            Buy Now
-          </button>
+          <p>
+            <strong>Pages: </strong>
+            {pages}
+          </p>
+          <p>
+            <strong>Points: </strong>
+            {points}
+          </p>
         </div>
       </div>
     </div>

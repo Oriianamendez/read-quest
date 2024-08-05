cae6945775015e0ab84076ece7fa18ca071ebb7b
Oriana Mendez <egleemendez@gmail.com>
2024-08-05 11:23:13 +0200

    gh-11 main page skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,46 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <>
+      <header>
+        <nav>
+          <p>Logo</p>
+          <p>Login</p>
+        </nav>
+      </header>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1>Welcome to ReadQuest</h1>
+        <p>This is a description for ReadQuest</p>
+        <article>
+          <section>
+            <h2>Book name</h2>
+            <p>Description of the book</p>
+            <p>Points</p>
+          </section>
+          <section>
+            <h2>Book name</h2>
+            <p>Description of the book</p>
+            <p>Points</p>
+          </section>
+          <section>
+            <h2>Book name</h2>
+            <p>Description of the book</p>
+            <p>Points</p>
+          </section>
+        </article>
+
+        <form action="">
+          <label>Book</label>
+          <input type="text" />
+          <label htmlFor="">Question</label>
+          <input type="text" />
+          <label htmlFor="">Question</label>
+          <input type="text" />
+          <label htmlFor="">Question</label>
+          <input type="text" />
+          <button>Get your points</button>
+        </form>
+      </main>
+    </>
   );
 }

146b7c3292f36471c920f9f69eafb642c0665f68
Oriana Mendez <egleemendez@gmail.com>
2024-08-05 13:29:11 +0200

    gh-8 description component

--- src/app/design-system/page.tsx
+++ src/app/design-system/page.tsx
@@ -1,5 +1,6 @@
 import { CTAButton, DeleteButton } from "@/components/buttons";
 import { BookCards } from "@/components/cards";
+import { Description } from "@/components/description";
 import { Form } from "@/components/form";
 import { TopNavPage } from "@/components/top-nav";
 
@@ -7,6 +8,7 @@ export default function DesignSystemPage() {
   return (
     <>
       <TopNavPage />
+      <Description />
       <CTAButton />
       <DeleteButton />
       <BookCards />

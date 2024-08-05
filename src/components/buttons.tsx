5766789651b0321f6724d9c463e02294ddb2b51f
Oriana Mendez <egleemendez@gmail.com>
2024-08-05 12:06:59 +0200

    gh-14 buttons component

---
+++ src/components/buttons.tsx
@@ -0,0 +1,7 @@
+export function CTAButton() {
+  return <button className="btn btn-accent">Get your points</button>;
+}
+
+export function DeleteButton() {
+  return <button className="btn btn-error">Error</button>;
+}

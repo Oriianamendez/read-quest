9fb9d813b56f793731d61b7a9beac71e2af46325
Oriana Mendez <egleemendez@gmail.com>
2024-08-12 11:45:20 +0200

    gh-12 user component skeleton

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,3 +1,24 @@
 export function User() {
-  return <h1>Welcome User!</h1>;
+  return (
+    <>
+      <nav>
+        <p>Here goes the TopNav</p>
+      </nav>
+      <div>
+        <header>
+          <h1>Welcome User!</h1>
+          <p>Slogan</p>
+        </header>
+        <div>
+          <p>Points</p>
+          <p>Books</p>
+        </div>
+        <main>
+          <p>Image?</p>
+          <p>Modal</p>
+          <p>Cards of books read</p>
+        </main>
+      </div>
+    </>
+  );
 }

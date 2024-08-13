232733ad88db6b73d5deb809ba217c5302e86037
Oriana Mendez <egleemendez@gmail.com>
2024-08-13 15:13:27 +0200

    gh-26 add value to select options

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,33 +1,44 @@
 "use client";
 
-import { handleQuestion } from "@/db/queries";
+import { getQuestions } from "@/db/queries";
 import { books } from "./books";
 import { CTAButton } from "./buttons";
 import { Form } from "./form";
+import { useState } from "react";
 
 export function ModalForm() {
+  const [book, setBook] = useState("");
+
   const showQuestions = () => {
+    "use client";
     (document.getElementById("my_modal_3") as HTMLDialogElement)?.showModal();
   };
 
   return (
     <main className="md:flex md:flex-col md:items-center pb-20">
-      <form action={handleQuestion}>
-        <select className="select select-warning w-full max-w-xs">
-          <option disabled selected>
-            Select your book
-          </option>
-          <option>{books.charlotteWebBook.name}</option>
-          <option>{books.theMagicTreeHouseBook.name}</option>
-          <option>{books.theBfgBook.name}</option>
-        </select>
-        <div>
-          <CTAButton
-            name={"Get your questions"}
-            onClick={() => showQuestions()}
-          />
-        </div>
-      </form>
+      <select
+        className="select select-warning w-full max-w-xs"
+        onChange={(e) => setBook(e.currentTarget.value)}
+        value={book}
+      >
+        <option value={"Select your book"}>Select your book</option>
+        <option value={books.charlotteWebBook.name}>
+          {books.charlotteWebBook.name}
+        </option>
+        <option value={books.theMagicTreeHouseBook.name}>
+          {books.theMagicTreeHouseBook.name}
+        </option>
+        <option value={books.theBfgBook.name}>{books.theBfgBook.name}</option>
+      </select>
+      <div>
+        <CTAButton
+          name={"Get your questions"}
+          onClick={async () => {
+            await getQuestions(book), showQuestions();
+          }}
+        />
+      </div>
+
       <dialog id="my_modal_3" className="modal">
         <div className="modal-box">
           <form method="dialog">

56d84dbf4c9a33aec5a0654096d14b587f2fe472
Oriana Mendez <egleemendez@gmail.com>
2024-08-13 15:12:04 +0200

    gh-25 use useEffect and useState

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,23 +1,28 @@
-import { charlotteWebBook } from "./books";
-import { CTAButton } from "./buttons";
-import { Label } from "./section";
-import { questionsCharlotteWeb } from "./questionary";
+import { useEffect, useState } from "react";
+import { Label, CTAButton } from "@/components";
+import { getQuestions } from "@/db/queries";
 
 export function Form() {
+  const [questions, setQuestions] = useState<any>([]);
+
+  useEffect(() => {
+    getQuestions("d1abbe98-c6a3-4dab-841b-e59ae8e7dfdc").then((data) => {
+      setQuestions(data);
+      console.log({ data });
+    });
+  }, []);
+
   return (
     <form action="" className="flex flex-col items-center">
-      <Label
-        placeholder={"Type the answer"}
-        information={questionsCharlotteWeb.Question1}
-      />
-      <Label
-        placeholder={"Type the answer"}
-        information={questionsCharlotteWeb.Question2}
-      />
-      <Label
-        placeholder={"Type the answer"}
-        information={questionsCharlotteWeb.Question3}
-      />
+      {questions.map((question: any) => {
+        return (
+          <Label
+            key={question.id}
+            placeholder={"Type the answer"}
+            information={question.questions}
+          />
+        );
+      })}
       <footer>
         <CTAButton name={"Get your points"} />
       </footer>

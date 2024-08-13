21d3bbf68d428842e0a65f5d5ae87b127b9fe36a
Oriana Mendez <egleemendez@gmail.com>
2024-08-13 15:15:21 +0200

    gh-27 fix and change getQuestion function

--- src/db/queries.ts
+++ src/db/queries.ts
@@ -1,29 +1,10 @@
 "use server";
 
 import { eq } from "drizzle-orm";
-import { db } from ".";
+import { db } from "./index";
 import { books, questions } from "./schema";
 
-export async function handleQuestion(formData: FormData) {
-  const book = formData.get("book") as string;
-  const question1 = formData.get("question1") as string;
-  const question2 = formData.get("question2") as string;
-  const question3 = formData.get("question3") as string;
-
-  const getBook = await db.query.books.findFirst({
-    where: eq(books.name, book),
+export const getQuestions = async (bookId: string) =>
+  await db.query.questions.findMany({
+    where: eq(questions.book_id, bookId),
   });
-  const getQuestions = await db.query.questions.findMany({
-    where: eq(books.id, questions.book_id),
-  });
-
-  if (book === getBook?.name) {
-    getQuestions.map((question) => {
-      return (
-        question1 === question.questions[0] &&
-        question2 === question.questions[1] &&
-        question3 === question.questions[2]
-      );
-    });
-  }
-}

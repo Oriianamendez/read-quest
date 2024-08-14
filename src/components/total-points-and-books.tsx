b873582cc48740f1e023d281f75590e16ca4e87f
Oriana Mendez <egleemendez@gmail.com>
2024-08-14 19:49:22 +0200

    gh-23 get total point

--- src/components/total-points-and-books.tsx
+++ src/components/total-points-and-books.tsx
@@ -1,8 +1,11 @@
-import { showTotalBooksRead } from "@/db/queries";
+"use client";
+
+import { getBookRead, showTotalBooksRead } from "@/db/queries";
 import { useEffect, useState } from "react";
 
 export function TotalPointsAndBooks() {
   const [data, setData] = useState<number>(0);
+  const [totalBooksRead, setTotalBooksRead] = useState<any>([]);
 
   useEffect(() => {
     showTotalBooksRead().then((result) => {
@@ -10,11 +13,28 @@ export function TotalPointsAndBooks() {
     });
   }, []);
 
+  useEffect(() => {
+    getBookRead("35895cdb-96ee-4828-8ef4-7e3ceb5a3048").then((result) => {
+      const totalBooks = result.map((bookRead) => {
+        return bookRead.book.points;
+      });
+      let sum = 0;
+      totalBooks.forEach((el) => (sum += el));
+
+      setTotalBooksRead(sum);
+    });
+  }, []);
+
   return (
     <>
       <div className="pr-32 py-6 md:text-xl text-xl font-semibold text-center">
-        <p>Points</p>
-        <p>
+        <p className="p-4">
+          Points
+          <span className="text-purple-700 bg-yellow-200 rounded-full p-2">
+            {totalBooksRead}
+          </span>
+        </p>
+        <p className="p-4">
           Books
           <span className="text-purple-700 bg-yellow-200 rounded-full p-2">
             {data}

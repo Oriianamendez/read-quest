9645fa126262ca919462373717121a31f3745bc8
Oriana Mendez <egleemendez@gmail.com>
2024-08-14 23:20:37 +0200

    add a type to button

--- src/components/buttons.tsx
+++ src/components/buttons.tsx
@@ -7,6 +7,7 @@ export function CTAButton({
 }) {
   return (
     <button
+      type="submit"
       className="btn bg-yellow-300 hover:bg-yellow-200 border-yellow-300 font-medium text-lg"
       onClick={onClick}
     >

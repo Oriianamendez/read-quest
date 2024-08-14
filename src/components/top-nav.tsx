44f2895dba17e79e90dc92c4bb8d7faf543fef3b
Oriana Mendez <egleemendez@gmail.com>
2024-08-14 09:15:35 +0200

    add new logo and create a new topnav

--- src/components/top-nav.tsx
+++ src/components/top-nav.tsx
@@ -28,3 +28,24 @@ export function TopNav() {
     </header>
   );
 }
+
+export function TopNavUser() {
+  return (
+    <header>
+      <nav className="navbar bg-yellow-200 ">
+        <div className="flex-1 justify-between">
+          <Link href={"/"} className="btn btn-ghost text-xl">
+            <Image src="/logo.png" width={45} height={45} alt="Logo" />
+          </Link>
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle avatar items-center"
+          >
+            <div className="w-10 rounded-full"></div>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}

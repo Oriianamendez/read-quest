aa679f41ab62e9ab589fd42b0ebef42a6d51827b
Oriana Mendez <egleemendez@gmail.com>
2024-08-14 10:22:04 +0200

    add newbookmodal component

--- src/components/top-nav.tsx
+++ src/components/top-nav.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { NewBookModal } from "./new-book-modal";
 
 export function TopNav() {
   return (
@@ -37,12 +38,10 @@ export function TopNavUser() {
           <Link href={"/"} className="btn btn-ghost text-xl">
             <Image src="/logo.png" width={45} height={45} alt="Logo" />
           </Link>
-          <div
-            tabIndex={0}
-            role="button"
-            className="btn btn-ghost btn-circle avatar items-center"
-          >
-            <div className="w-10 rounded-full"></div>
+          <div tabIndex={0} role="button" className="flex items-center ">
+            <div className="">
+              <NewBookModal />
+            </div>
           </div>
         </div>
       </nav>

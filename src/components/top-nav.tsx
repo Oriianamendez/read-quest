b97b5459a1c819b327c6dded29f7b619fa2c6efb
Oriana Mendez <egleemendez@gmail.com>
2024-08-15 22:42:29 +0200

    styling

--- src/components/top-nav.tsx
+++ src/components/top-nav.tsx
@@ -7,7 +7,7 @@ import { NewBookModal } from "./new-book-modal";
 export function TopNav() {
   return (
     <header>
-      <nav className="navbar bg-yellow-200 ">
+      <nav className="navbar bg-white ">
         <div className="flex-1 justify-between">
           <Link href={"/"} className="btn btn-ghost text-xl">
             <Image src="/logo.png" width={45} height={45} alt="Logo" />

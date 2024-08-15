c51b7139b0d3bbd3b138a32a58e79b1912a89b4b
Oriana Mendez <egleemendez@gmail.com>
2024-08-15 14:40:18 +0200

    change checkbox title

--- src/components/checkbox.tsx
+++ src/components/checkbox.tsx
@@ -2,7 +2,7 @@ export function Checkbox() {
   return (
     <div className="form-control align-bottom pt-2">
       <label className="cursor-pointer label">
-        <span className="label-text pr-2 text-lg text-slate-600">Waiting</span>
+        <span className="label-text pr-2 text-lg text-slate-600">Approval</span>
         <input
           type="checkbox"
           className="checkbox checkbox-warning bg-yellow-100"

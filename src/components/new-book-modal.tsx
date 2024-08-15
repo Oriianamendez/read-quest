c1ee543746cec56341fede93c77e12fc0c79d1cc
Oriana Mendez <egleemendez@gmail.com>
2024-08-15 22:43:47 +0200

    change placeholders title

--- src/components/new-book-modal.tsx
+++ src/components/new-book-modal.tsx
@@ -55,16 +55,20 @@ function NewBookForm() {
 
   return (
     <form onSubmit={handleSubmit} className="flex flex-col items-center">
-      <Label placeholder="Book title" information="Title" name="name" />
-      <Label placeholder="Book author" information="Author" name="author" />
       <Label
-        placeholder="Brief description of the book"
-        information="Description"
+        placeholder="Harry Potter and the Sorcerer's..."
+        information="Book title"
+        name="name"
+      />
+      <Label placeholder="J.K. Rowling" information="Author" name="author" />
+      <Label
+        placeholder="Harry Potter has no idea how..."
+        information="Brief description of the book"
         name="description"
       />
       <Label placeholder="Pages" information="Pages" name="pages" />
       <Label placeholder="Points" information="Points" name="points" />
-      <Label placeholder="From which age" information="Age" name="age" />
+      <Label placeholder="Min reading age" information="Age" name="age" />
       <footer className="mt-4">
         <CTAButton name={"Add new book"} />
       </footer>
